3818e75f83c735b8e7dfd5f23aee1744306c0858
Jeremy <laplumaencre@gmail.com>
2017-06-11 22:35:32 +0200

    Add files via upload

---
+++ option_langues.php
@@ -0,0 +1,193 @@
+<option class="option_lang" value='aa'>Afaraf</option><!-- Afar -->
+<option class="option_lang" value='ab'>Аҧсуа</option><!-- Abkhaz -->
+<option class="option_lang" value='ae'>Avesta</option><!-- Avestan -->
+<option class="option_lang" value='af'>Afrikaans</option><!-- Afrikaans -->
+<option class="option_lang" value='ak'>Akan</option><!-- Akan -->
+<option class="option_lang" value='am'>አማርኛ</option><!-- Amharic -->
+<option class="option_lang" value='an'>Aragonés</option><!-- Aragonese -->
+<option class="option_lang" value='ar'>العربية</option><!-- Arabic -->
+<option class="option_lang" value='as'>অসমীয়া</option><!-- Assamese  -->
+<option class="option_lang" value='av'>магӀарул мацӀ</option><!-- Avaric -->
+<option class="option_lang" value='ay'>Aymar aru</option><!-- Aymara -->
+<option class="option_lang" value='az'>Azərbaycan dili</option><!-- Azerbaijani -->
+
+<option class="option_lang" value='ba'>башҡорт теле</option><!-- Bashkir -->
+<option class="option_lang" value='be'>Беларуская</option><!-- Belarusian -->
+<option class="option_lang" value='bg'>български език</option><!-- Bulgarian -->
+<option class="option_lang" value='bh'>भोजपुरी</option><!-- Bihari -->
+<option class="option_lang" value='bi'>Bislama</option><!-- Bislama -->
+<option class="option_lang" value='bm'>bamanankan</option><!-- Bambara -->
+<option class="option_lang" value='bn'>বাংলা</option><!-- Bengali -->
+<option class="option_lang" value='bo'>བོད་ཡིག</option><!-- Standard Tibetan -->
+<option class="option_lang" value='br'>Brezhoneg</option><!-- Breton -->
+<option class="option_lang" value='bs'>Bosanski jezik</option><!-- Bosnian -->
+
+<option class="option_lang" value='ca'>Català</option><!-- Catalan -->
+<option class="option_lang" value='ce'>нохчийн мотт</option><!-- Chechen -->
+<option class="option_lang" value='co'>Corsu</option><!-- Corsican -->
+<option class="option_lang" value='cr'>ᓀᐦᐃᔭᐍᐏᐣ</option><!-- Cree -->
+<option class="option_lang" value='cs'>čeština</option><!-- Czech -->
+<option class="option_lang" value='cv'>чӑваш чӗлхи</option><!-- Chuvash -->
+<option class="option_lang" value='cy'>Cymraeg</option><!-- Welsh -->
+
+<option class="option_lang" value='da'>Dansk</option><!-- Danish -->
+<option class="option_lang" value='de'>Deutsch</option><!-- German -->
+<option class="option_lang" value='dv'>Dihevi</option><!-- Maldivian -->
+<option class="option_lang" value='dz'>རྫོང་ཁ་</option><!-- Dzongkha -->
+
+<option class="option_lang" value='ee'>Eʋegbe</option><!-- Ewe -->
+<option class="option_lang" value='el'>Ελληνικά</option><!-- Greek -->
+<option class="option_lang" value='en'>English</option>
+<option class="option_lang" value='eo'>Esperanto</option><!-- Esperanto -->
+<option class="option_lang" value='es'>Español</option><!-- Spanish -->
+<option class="option_lang" value='et'>Eesti</option><!-- Estonian -->
+<option class="option_lang" value='eu'>Euskara</option><!-- Basque -->
+
+<option class="option_lang" value='fa'>فارسی</option><!-- Persian -->
+<option class="option_lang" value='ff'>Fulfulde</option><!-- Fulah -->
+<option class="option_lang" value='fi'>Suomi</option><!-- Finnish -->
+<option class="option_lang" value='fj'>vosa Vakaviti</option><!-- Fijian -->
+<option class="option_lang" value='fo'>Føroyskt</option><!-- Faroese -->
+<option class="option_lang" value='fr'>Français</option><!-- French -->
+<option class="option_lang" value='fy'>Frysk</option><!-- Western Frisian -->
+
+<option class="option_lang" value='ga'>Gaeilge</option><!-- Irish -->
+<option class="option_lang" value='gd'>Gàidhlig</option><!-- Scottish Gaelic -->
+<option class="option_lang" value='gl'>Galego</option><!-- Galician -->
+<option class="option_lang" value='gn'>Avañeẽ</option><!-- Guaraní -->
+<option class="option_lang" value='gu'>ગુજરાતી</option><!-- Gujarati -->
+<option class="option_lang" value='gv'>Gaelg</option><!--Manx  -->
+
+<option class="option_lang" value='ha'>هَوُسَ</option><!-- Hausa -->
+<option class="option_lang" value='he'>עברית</option><!-- Hebrew -->
+<option class="option_lang" value='hi'>हिन्दी</option><!-- Hindi -->
+<option class="option_lang" value='ho'>Hiri Motu</option><!-- Hiri Motu -->
+<option class="option_lang" value='hu'>Magyar</option><!-- Hungarian -->
+<option class="option_lang" value='hr'>Hrvatski</option><!-- Croatian -->
+<option class="option_lang" value='ht'>Kreyòl</option><!-- Haitian Creole -->
+<option class="option_lang" value='hy'>Հայերեն</option><!-- Armenian -->
+<option class="option_lang" value='hz'>Otjiherero</option><!-- Herero -->
+
+<option class="option_lang" value='id'>Bahasa Indonesia</option><!-- Indonesian -->
+<option class="option_lang" value='ig'>Asụsụ Igbo</option><!-- Igbo -->
+<option class="option_lang" value='ik'>Iñupiatun</option><!-- Inupiaq -->
+<option class="option_lang" value='io'>Ido</option><!-- Ido -->
+<option class="option_lang" value='is'>Íslenska</option><!-- Icelandic -->
+<option class="option_lang" value='it'>Italiano</option><!-- Italian -->
+<option class="option_lang" value='iu'>ᐃᓄᒃᑎᑐᑦ</option><!-- Inuktitut -->
+
+<option class="option_lang" value='ja'>日本語</option><!-- Japanese -->
+<option class="option_lang" value='jv'>basa Jawa</option><!-- Javanese -->
+
+<option class="option_lang" value='ka'>ქართული</option><!-- Georgian -->
+<option class="option_lang" value='kg'>KiKongo</option><!-- Kongo -->
+<option class="option_lang" value='ki'>Gĩkũyũ</option><!-- Kikuyu -->
+<option class="option_lang" value='kj'>Oshikwanyama</option><!-- Kwanyama -->
+<option class="option_lang" value='kk'>Қазақ тілі</option><!-- Kazakh -->
+<option class="option_lang" value='kl'>Kalaallisut</option><!-- Greenlandic -->
+<option class="option_lang" value='km'>ភាសាខ្មែរ</option><!-- Khmer -->
+<option class="option_lang" value='ko'>한국어</option><!-- Korean -->
+<option class="option_lang" value='kr'>Kanuri</option><!-- Kanuri -->
+<option class="option_lang" value='ks'>कॉशुर</option><!-- Kashmiri -->
+<option class="option_lang" value='ku'>کوردی</option><!-- Kurdish -->
+<option class="option_lang" value='kv'>коми кыв</option><!-- Komi -->
+<option class="option_lang" value='kw'>Kernewek</option><!-- Cornish -->
+<option class="option_lang" value='ky'>Кыргызча</option><!-- Kyrgyz -->
+
+<option class="option_lang" value='lb'>Lëtzebuergesch</option><!-- Luxembourgish -->
+<option class="option_lang" value='ln'>Lingála</option><!-- Lingala -->
+<option class="option_lang" value='lo'>ພາສາລາວ</option><!-- Lao -->
+<option class="option_lang" value='lt'>lietuvių kalba</option><!-- Lithuanian -->
+<option class="option_lang" value='lu'>Tshiluba</option><!-- Luba-Katanga -->
+<option class="option_lang" value='lv'>Latviešu valoda</option><!-- Latvian -->
+
+<option class="option_lang" value='mg'>Malagasy fiteny</option><!-- Malagasy -->
+<option class="option_lang" value='mi'>te reo Māori</option><!-- Māori -->
+<option class="option_lang" value='mk'>македонски јазик</option><!-- Macedonian -->
+<option class="option_lang" value='ml'>മലയാളം</option><!-- Malayalam -->
+<option class="option_lang" value='mn'>монгол</option><!-- Mongolian -->
+<option class="option_lang" value='mr'>मराठी</option><!-- Marathi -->
+<option class="option_lang" value='ms'>بهاس ملايو</option><!-- Malay -->
+<option class="option_lang" value='mt'>Malti</option><!-- Maltese -->
+<option class="option_lang" value='my'>ဗမာစာ</option><!-- Burmese -->
+
+<option class="option_lang" value='na'>Ekakairũ Naoero</option><!-- Nauru -->
+<option class="option_lang" value='ne'>नेपाली</option><!-- Nepali -->
+<option class="option_lang" value='ng'>Owambo</option><!--Ndonga  -->
+<option class="option_lang" value='nl'>Nederlands</option><!-- Dutch -->
+<option class="option_lang" value='no'>Norsk</option><!-- Norwegian -->
+<option class="option_lang" value='nv'>Diné bizaad</option><!-- Navajo -->
+<option class="option_lang" value='ny'>ChiCheŵa</option><!-- Chichewa -->
+
+<option class="option_lang" value='oc'>Occitan</option><!-- Occitan -->
+<option class="option_lang" value='oj'>ᐊᓂᔑᓈᐯᒧᐎᓐ</option><!-- Ojibwe -->
+<option class="option_lang" value='om'>Afaan Oromoo</option><!-- Oromo -->
+<option class="option_lang" value='or'>ଓଡ଼ିଆ</option><!-- Oriya -->
+<option class="option_lang" value='os'>ирон æвзаг</option><!-- Ossetian -->
+
+<option class="option_lang" value='pa'>ਪੰਜਾਬੀ</option><!-- Punjabi -->
+<option class="option_lang" value='pi'>पाऴि</option><!-- Pāli -->
+<option class="option_lang" value='pl'>Polski</option><!-- Polish -->
+<option class="option_lang" value='ps'>پښتو</option><!-- Pashto -->
+<option class="option_lang" value='pt'>Português</option><!-- Portuguese -->
+
+<option class="option_lang" value='qu'>Runa simi</option><!-- Quechua -->
+
+<option class="option_lang" value='rm'>Rumantsch grischun</option><!-- Romansh -->
+<option class="option_lang" value='rn'>Ikirundi</option><!-- Rundi -->
+<option class="option_lang" value='ro'>Română</option><!-- Romanian -->
+<option class="option_lang" value='ru'>русский язык</option><!-- Russian -->
+<option class="option_lang" value='rw'>Ikinyarwanda</option><!-- Kinyarwanda -->
+
+<option class="option_lang" value='sa'>संस्कृतम्</option><!-- Sanskrit -->
+<option class="option_lang" value='sc'>sardu</option><!-- Sardinian -->
+<option class="option_lang" value='sd'>سنڌي</option><!-- Sindhi -->
+<option class="option_lang" value='se'>Davvisámegiella</option><!-- Northern Sami -->
+<option class="option_lang" value='sg'>Yângâ tî sängö</option><!-- Sango -->
+<option class="option_lang" value='si'>සිංහල</option><!-- Sinhalese -->
+<option class="option_lang" value='sk'>Slovenčina</option><!-- Slovak -->
+<option class="option_lang" value='sl'>Slovenščina</option><!-- Slovenian -->
+<option class="option_lang" value='sm'>Gagana faa Samoa</option><!-- Samoan -->
+<option class="option_lang" value='sn'>chiShona</option><!-- Shona -->
+<option class="option_lang" value='so'>Af-Soomaali</option><!-- Somali -->
+<option class="option_lang" value='sq'>Shqip</option><!-- Albanian -->
+<option class="option_lang" value='sr'>Cрпски језик</option><!-- Serbian -->
+<option class="option_lang" value='ss'>SiSwati</option><!-- Swati -->
+<option class="option_lang" value='st'>Sesotho</option><!-- Southern Sotho -->
+<option class="option_lang" value='su'>Basa Sunda</option><!-- Sundanese -->
+<option class="option_lang" value='sv'>Svenska</option><!-- Swedish -->
+<option class="option_lang" value='sw'>Kiswahili</option><!-- Swahili -->
+
+<option class="option_lang" value='ta'>தமிழ்</option><!-- Tamil -->
+<option class="option_lang" value='th'>ไทย</option><!-- Thai -->
+<option class="option_lang" value='ti'>ትግርኛ</option><!-- Tigrinya -->
+<option class="option_lang" value='tk'>Түркмен</option><!-- Turkmen -->
+<option class="option_lang" value='tl'>Wikang Tagalog</option><!-- Tagalog -->
+<option class="option_lang" value='tn'>Setswana</option><!-- Tswana -->
+<option class="option_lang" value='to'>faka Tonga</option><!-- Tongan -->
+<option class="option_lang" value='tr'>Türkçe</option><!-- Turkish -->
+<option class="option_lang" value='ts'>Xitsonga</option><!-- Tsonga -->
+<option class="option_lang" value='tt'>татар теле</option><!-- Tatar -->
+<option class="option_lang" value='tw'>Twi</option><!-- Twi -->
+<option class="option_lang" value='ty'>Reo Tahiti</option><!-- Tahitian -->
+
+<option class="option_lang" value='ug'>ۇيغۇر تىلى</option><!-- Uyghur -->
+<option class="option_lang" value='uk'>українська</option><!-- Ukrainian -->
+<option class="option_lang" value='ur'>اردو</option><!-- Urdu -->
+<option class="option_lang" value='uz'>Oʻzbekcha</option><!-- Uzbek -->
+
+<option class="option_lang" value='ve'>Tshivenḓa</option><!-- Venda -->
+<option class="option_lang" value='vi'>Tiếng Việt</option><!-- Vietnamese -->
+<option class="option_lang" value='vo'>Volapük</option><!-- Volapük -->
+
+
+<option class="option_lang" value='wa'>Walon</option><!-- Walloon -->
+<option class="option_lang" value='wo'>Wollof</option><!-- Wollof -->
+
+<option class="option_lang" value='xh'>isiXhosa</option><!-- Xhosa -->
+
+<option class="option_lang" value='yi'>ייִדיש</option><!-- Yiddish -->
+<option class="option_lang" value='yo'>Yorùbá</option><!-- Yoruba -->
+
+<option class="option_lang" value='za'>Vaƅcueŋƅ</option><!-- Zhuang -->
+<option class="option_lang" value='zh'>中文</option><!-- Chinese -->
